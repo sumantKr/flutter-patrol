6a9fff5a259c0da073d8512779c0889e4130d30a
sumantKr <sumantkumar172000@gmail.com>
2022-10-04 01:05:44 +0530

    first commit

---
+++ integration_test/config.dart
@@ -0,0 +1,8 @@
+import 'package:patrol/patrol.dart';
+
+// TODO: Replace with values specific to your app.
+const patrolConfig = PatrolTestConfig(
+  appName: 'Example App',
+  packageName: 'pl.leancode.patrol.example',
+  bundleId: 'pl.leancode.patrol.Example',
+);
